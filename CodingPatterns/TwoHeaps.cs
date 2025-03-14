9a5bd045c7ff92157b8a98376c5eecb85d673682
srielango <srielango@gmail.com>
2025-03-14 16:43:03 +0530

    Added TwoHeap Coding pattern

--- CodingPatterns/TwoHeaps.cs
+++ CodingPatterns/TwoHeaps.cs
@@ -1,13 +1,92 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace CodingPatterns
 {
     public class TwoHeaps
     {
-        
+        private readonly IMedianStrategy _medianStrategy;
+
+        public TwoHeaps(IMedianStrategy medianStrategy)
+        {
+            _medianStrategy = medianStrategy;
+        }
+
+        public void AddNum(int num)
+        {
+            _medianStrategy.AddNum(num);
+        }
+
+        public double FindMedian()
+        {
+            return _medianStrategy.FindMedian();
+        }
+    }
+
+    public interface IMedianStrategy
+    {
+        public void AddNum(int num);
+        public double FindMedian();
+    }
+
+    //This is good for small set of numbers.  For streaming large numbers this will fail
+    public class NaiveMedianStrategy : IMedianStrategy
+    {
+        private List<int> MyList = new();
+        public void AddNum(int num)
+        {
+            MyList.Add(num);
+        }
+
+        public double FindMedian()
+        {
+            MyList.Sort();
+            var count = MyList.Count();
+            int middleIndex = count / 2;
+            if(count % 2 != 0)
+            {
+                return MyList[middleIndex];
+            }
+            return ((double)MyList[middleIndex - 1] + (double)MyList[middleIndex]) / 2;
+        }
+    }
+
+    public class TwoHeapStrategy : IMedianStrategy
+    {
+        PriorityQueue<int, int> MinHeap = new();
+        PriorityQueue<int, int> MaxHeap = new(Comparer<int>.Create((x, y) => y.CompareTo(x)));
+
+        public void AddNum(int num)
+        {
+            MaxHeap.TryPeek(out int peekValue, out int peekPriority);
+            if (peekValue > num)
+            {
+                MaxHeap.Enqueue(num, num);
+            }
+            else
+            {
+                MinHeap.Enqueue(num, num);
+            }
+
+            if(MaxHeap.Count > MinHeap.Count + 1)
+            {
+                var maxNum = MaxHeap.Dequeue();
+                MinHeap.Enqueue(maxNum, maxNum);
+            }
+            if(MinHeap.Count > MaxHeap.Count)
+            {
+                var minNum = MinHeap.Dequeue();
+                MaxHeap.Enqueue(minNum, minNum);
+            }
+        }
+
+        public double FindMedian()
+        {
+            var elementCount = MinHeap.Count + MaxHeap.Count;
+            if (elementCount % 2 != 0)
+            {
+                return MaxHeap.Peek();
+            }
+            return ((double)MaxHeap.Peek() + (double)MinHeap.Peek()) / 2;
+        }
     }
 }
---
+++ CodingPatternsTests/TwoHeapsTests.cs
@@ -0,0 +1,48 @@
+﻿using CodingPatterns;
+using FluentAssertions;
+
+namespace CodingPatternsTests
+{
+    [TestClass]
+    public class TwoHeapsTests
+    {
+        private readonly TwoHeaps _sut;
+
+        public TwoHeapsTests()
+        {
+            //_sut = new TwoHeaps(new NaiveMedianStrategy());
+            _sut = new TwoHeaps(new TwoHeapStrategy());
+        }
+
+        [TestMethod]
+        public void OddCount_ShouldReturnMiddleValue()
+        {
+            _sut.AddNum(1);
+            _sut.AddNum(2);
+            _sut.AddNum(3);
+
+            var result = _sut.FindMedian();
+            result.Should().Be(2);
+
+            _sut.AddNum(4);
+            _sut.AddNum(5);
+            result = _sut.FindMedian();
+            result.Should().Be(3);
+        }
+
+        [TestMethod]
+        public void EvenCount_ShouldReturnAverageOfMiddleValues()
+        {
+            _sut.AddNum(1);
+            _sut.AddNum(2);
+
+            var result = _sut.FindMedian();
+            result.Should().Be(1.5);
+
+            _sut.AddNum(4);
+            _sut.AddNum(5);
+            result = _sut.FindMedian();
+            result.Should().Be(3);
+        }
+    }
+}
